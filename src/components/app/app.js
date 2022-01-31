12ad4db785cc83af6e83f4159c5c0ff41d6d662f
Irina <hallo.ween@mail.ru>
2022-01-31 15:45:10 +0300

    lesson2

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,34 @@
 import './app.scss';
 import Message from '../message/message';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  const messageText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, dolorem similique.';
+  const [messageList, setMessageList] = useState([]);
+  const [newMessageText, setNewMessageText] = useState('');
+
+  useEffect(() => {
+    if (messageList.length > 0 && messageList[messageList.length - 1].author === "Me") {
+      const botAnswerInterval = setTimeout(() => setMessageList(messageList => [...messageList, { author: 'Bot', text: 'Message from Bot' }]), 1500);
+      return () => clearTimeout(botAnswerInterval);
+    }
+  }, [messageList]);
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+    setMessageList([...messageList, { author: 'Me', text: newMessageText }]);
+    setNewMessageText("");
+  };
+
+  const messageListRender = messageList.map(el => <Message key={el.author + Math.random()} messageText={el.text} author={el.author} />);
 
   return (
     <div className='app'>
-      <Message messageText={messageText}/>
+      {messageListRender}
+      <form className="app-form" onSubmit={onHandleSubmit}>
+        <input className="app-message" value={newMessageText} onChange={e => setNewMessageText(e.target.value)}></input>
+        <button className="app-button">Send</button>
+      </form>
     </div>
   );
 }
