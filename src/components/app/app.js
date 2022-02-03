6401ed8d6a12009a61a53296a37a25f7421387e9
Irina <hallo.ween@mail.ru>
2022-02-03 15:04:36 +0300

    fix after review

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,28 @@
 import './app.scss';
-import Message from '../message/message';
 import { useEffect, useState } from 'react';
+import { AUTHORS } from '../../utils/constants';
+import MessageList from '../messegeList/messageList';
+import Form from './../form/form';
 
 function App() {
 
   const [messageList, setMessageList] = useState([]);
-  const [newMessageText, setNewMessageText] = useState('');
 
   useEffect(() => {
-    if (messageList.length > 0 && messageList[messageList.length - 1].author === "Me") {
-      const botAnswerInterval = setTimeout(() => setMessageList(messageList => [...messageList, { author: 'Bot', text: 'Message from Bot' }]), 1500);
+    if (messageList[messageList.length - 1]?.author === AUTHORS.AUTHOR_ME) {
+      const botAnswerInterval = setTimeout(() => sendMessage(AUTHORS.AUTHOR_BOT, 'Message from Bot'), 1500);
       return () => clearTimeout(botAnswerInterval);
     }
   }, [messageList]);
 
-  const onHandleSubmit = (e) => {
-    e.preventDefault();
-    setMessageList([...messageList, { author: 'Me', text: newMessageText }]);
-    setNewMessageText("");
+  const sendMessage = (author, text) => {
+    setMessageList(messageList => [...messageList, { author: author, text: text }]);
   };
-
-  const messageListRender = messageList.map(el => <Message key={el.author + Math.random()} messageText={el.text} author={el.author} />);
 
   return (
     <div className='app'>
-      {messageListRender}
-      <form className="app-form" onSubmit={onHandleSubmit}>
-        <input className="app-message" value={newMessageText} onChange={e => setNewMessageText(e.target.value)}></input>
-        <button className="app-button">Send</button>
-      </form>
+      <MessageList messageList={messageList} />
+      <Form sendMessage={sendMessage} />
     </div>
   );
 }
--- src/components/message/message.js
+++ src/components/message/message.js
@@ -4,7 +4,6 @@ const Message = ({messageText, author}) => {
     return (
         <div className='message'>
             <p className={'message-author ' + (author === 'Bot' ? 'message-author__bot' : 'message-author__user')}>{author}</p>
-            {/* <p className='message-text__user'>{author}</p> */}
             <p className={'message-text ' + (author === 'Bot' ? 'message-text__bot' : 'message-text__user')}>{messageText}</p>
         </div>
     )
